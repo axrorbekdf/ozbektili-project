a5ba5d02469ed0365c843681fc0f71380a0112e2
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-19 00:30:49 +0500

    main page

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy new'
-git checkout gh-pages
  
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -456,19 +456,19 @@
       </div>
     </section>
 
-    <section class="container-fluid p-0 mt-5" style="background-color: #062A49">
+    <section class="container-fluid p-0 mt-5 d-none" style="background-color: #062A49">
       <div class="container py-5">
         <div class="row justify-content-between py-5">
-          <div class="col-6 text-white" style="display: flex; flex-direction: column;">
+          <div class="col-md-6 col-12 text-white" style="display: flex; flex-direction: column;">
             <img src="@/assets/logo-white.svg" width="250" alt="">  
             <p class="mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto fugit minus temporibus quae vitae ullam rem repellendus consequatur?</p>
           </div>
-          <div class="col-3 text-white" style="display: flex; flex-direction: column;">
+          <div class="col-md-3 col-12  text-white" style="display: flex; flex-direction: column;">
             <h4>O'zbek tili darajalari.</h4>
             <p class="text-secondary mt-2 my-1">Beginner</p>
             <p class="text-secondary">Elementary</p>
           </div>
-          <div class="col-3 text-white" style="display: flex; flex-direction: column;">
+          <div class="col-md-3 col-12 text-white" style="display: flex; flex-direction: column;">
             <h4>Bizning kontaktlarimiz!</h4>
             <p class="text-secondary mt-2 my-1">
               <img src="@/assets/phone-solid.svg" alt="">  
@@ -555,7 +555,7 @@
 
   .video-old-block{
     height: 0;
-    margin-bottom: 13.2vh;
+    margin-bottom: 15vh;
   }
   .video-old-block h1{
     font-size: 18px;
