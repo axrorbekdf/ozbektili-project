a9bbd3d5d66519c76ad4cd3d495076d655396693
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-19 12:09:22 +0500

    main page

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy new'
+git commit -m 'deploy background'
  
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
