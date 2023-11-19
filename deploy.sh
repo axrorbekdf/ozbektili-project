86234b14290f271270cbd91462ca7860401f471f
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-19 12:19:26 +0500

    deploy github pages

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy background'
  
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:axrorbekdf/axrorbekdf.github.io.git master
+# git push -f git@github.com:axrorbekdf/axrorbekdf.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:axrorbekdf/ozbektili-project.git master:gh-pages
