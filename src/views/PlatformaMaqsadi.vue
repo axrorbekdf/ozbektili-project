8e2609d8d61df2b8259c98553ac6738bda78d01d
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-21 14:53:04 +0500

    deploy github pages

--- src/views/PlatformaMaqsadi.vue
+++ src/views/PlatformaMaqsadi.vue
@@ -1,52 +1,66 @@
 <template lang="">
-    <section data-section-id="1" data-share="custom-380888" data-category="faqs" data-component-id="c0b652c1_01_awz" data-custom-component-id="380888" class="position-relative py-20" data-path="0"><img class="d-none d-lg-block position-absolute top-0 start-0 mt-16" src="https://shuffle.dev/zeus-assets/icons/dots/blue-dot-left-bars.svg" alt="" data-config-id="image1" data-path="0.0"><img class="d-none d-lg-block position-absolute bottom-0 end-0 mb-20" src="https://shuffle.dev/zeus-assets/icons/dots/yellow-dot-right-shield.svg" alt="" data-config-id="image2" data-path="0.1"><div class="container" data-path="0.2">
-          <div class="row" data-path="0.2.0">
-            <div class="col-12 col-lg-8 mx-auto text-center mb-12" data-path="0.2.0.0">
-              <span class="small text-info fw-bold" data-config-id="text15" data-path="0.2.0.0.0">What's new at Shuffle</span>
-              <h2 class="mt-8 mb-10" data-config-id="text8" data-path="0.2.0.0.1">Lorem ipsum dolor sit amet consectutar domor at elis</h2>
-              <p class="lead text-muted" data-config-id="text9" data-path="0.2.0.0.2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa nibh, pulvinar vitae aliquet nec, accumsan aliquet orci.</p>
-            </div>
-            <div class="d-block mb-6 mb-md-14 mx-auto text-center border-md-bottom" data-path="0.2.0.1">
-              <button class="bg-transparent border-0 pb-4 mb-2 mb-md-0 me-4 text-muted border-bottom border-transparent fw-bold" data-config-id="text21" data-path="0.2.0.1.0">General</button>
-              <button class="bg-transparent border-top-0 border-start-0 border-end-0 pb-4 mb-2 mb-md-0 me-4 border-bottom border-danger fw-bold" data-config-id="text22" data-path="0.2.0.1.1">Payments</button>
-              <button class="bg-transparent border-0 pb-4 mb-2 mb-md-0 me-4 text-muted border-bottom border-transparent fw-bold" data-config-id="text23" data-path="0.2.0.1.2">Returns</button>
-              <button class="bg-transparent border-0 pb-4 text-muted border-bottom border-transparent fw-bold" data-config-id="text24" data-path="0.2.0.1.3">Refunds</button>
-            </div>
-            <div class="max-w-3xl mx-auto" data-path="0.2.0.2">
-              <ul class="list-unstyled" data-path="0.2.0.2.0"><li class="px-6 py-8 border-bottom" data-path="0.2.0.2.0.0">
-                  <button class="w-100 bg-transparent border-0 d-flex justify-content-between align-items-center text-start text-dark" data-path="0.2.0.2.0.0.0">
-                    <span class="h4" data-config-id="text16" data-path="0.2.0.2.0.0.0.0">Fusce eget nunc et libero accumsan rutrum quis nec lectus?</span>
-                    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="svg-inline3" data-path="0.2.0.2.0.0.0.1"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2498 10C19.2498 4.892 15.1088 0.75 9.99976 0.75C4.89176 0.75 0.749756 4.892 0.749756 10C0.749756 15.108 4.89176 19.25 9.99976 19.25C15.1088 19.25 19.2498 15.108 19.2498 10Z" stroke="#45C1FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.0.0.1.0"></path><path d="M13.4709 11.4423L9.99995 7.95626L6.52895 11.4423" stroke="#45C1FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.0.0.1.1"></path></svg></button>
-                  <p class="mt-4 text-muted" data-config-id="text10" data-path="0.2.0.2.0.0.1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod orci sed tristique placerat. Fusce in ligula urna. Fusce eget nunc et libero accumsan rutrum quis nec lectus. Quisque luctus sem nibh, quis ornare neque consectetur varius. Maecenas rhoncus consectetur rutrum.</p>
-                </li>
-                <li class="px-6 py-8 border-bottom" data-path="0.2.0.2.0.1">
-                  <button class="w-100 bg-transparent border-0 d-flex justify-content-between align-items-center text-start text-dark" data-path="0.2.0.2.0.1.0">
-                    <span class="h4" data-config-id="text17" data-path="0.2.0.2.0.1.0.0">Donec sed leo sit amet ante ornare laoreet in quis est?</span>
-                    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="svg-inline4" data-path="0.2.0.2.0.1.0.1"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.750244 10C0.750244 15.108 4.89124 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89124 0.75 0.750244 4.892 0.750244 10Z" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.1.0.1.0"></path><path d="M6.52905 8.55768L10.0001 12.0437L13.4711 8.55768" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.1.0.1.1"></path></svg></button>
-                  <p class="d-none mt-4 text-muted" data-config-id="text11" data-path="0.2.0.2.0.1.1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ultricies est. Duis nec hendrerit magna. Ut vel orci gravida, hendrerit enim non, gravida turpis. Praesent non lectus porttitor, scelerisque nulla nec, ornare neque. Integer massa libero, ornare ut leo nec, scelerisque rutrum elit. Morbi et massa id ipsum lobortis rhoncus. Aenean sit amet porta nulla, quis varius risus. Suspendisse ultrices ut lectus non laoreet. Etiam ornare laoreet tortor quis porttitor. Suspendisse tempus erat non dui volutpat eleifend.</p>
-                </li>
-                <li class="px-6 py-8 border-bottom" data-path="0.2.0.2.0.2">
-                  <button class="w-100 bg-transparent border-0 d-flex justify-content-between align-items-center text-start text-dark" data-path="0.2.0.2.0.2.0">
-                    <span class="h4" data-config-id="text18" data-path="0.2.0.2.0.2.0.0">Mauris vitae ex ut lectus cursus ornare?</span>
-                    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="svg-inline5" data-path="0.2.0.2.0.2.0.1"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.750244 10C0.750244 15.108 4.89124 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89124 0.75 0.750244 4.892 0.750244 10Z" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.2.0.1.0"></path><path d="M6.52905 8.55768L10.0001 12.0437L13.4711 8.55768" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.2.0.1.1"></path></svg></button>
-                  <p class="d-none mt-4 text-muted" data-config-id="text12" data-path="0.2.0.2.0.2.1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis ultricies est. Duis nec hendrerit magna. Ut vel orci gravida, hendrerit enim non, gravida turpis. Praesent non lectus porttitor, scelerisque nulla nec, ornare neque. Integer massa libero, ornare ut leo nec, scelerisque rutrum elit. Morbi et massa id ipsum lobortis rhoncus. Aenean sit amet porta nulla, quis varius risus. Suspendisse ultrices ut lectus non laoreet. Etiam ornare laoreet tortor quis porttitor. Suspendisse tempus erat non dui volutpat eleifend.</p>
-                </li>
-                <li class="px-6 py-8 border-bottom" data-path="0.2.0.2.0.3">
-                  <button class="w-100 bg-transparent border-0 d-flex justify-content-between align-items-center text-start text-dark" data-path="0.2.0.2.0.3.0">
-                    <span class="h4" data-config-id="text19" data-path="0.2.0.2.0.3.0.0">Nam consequat, augue sed rutrum faucibus?</span>
-                    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="svg-inline6" data-path="0.2.0.2.0.3.0.1"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.750244 10C0.750244 15.108 4.89124 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89124 0.75 0.750244 4.892 0.750244 10Z" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.3.0.1.0"></path><path d="M6.52905 8.55768L10.0001 12.0437L13.4711 8.55768" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.3.0.1.1"></path></svg></button>
-                  <p class="d-none mt-4 text-muted" data-config-id="text13" data-path="0.2.0.2.0.3.1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod orci sed tristique placerat. Fusce in ligula urna. Fusce eget nunc et libero accumsan rutrum quis nec lectus. Quisque luctus sem nibh, quis ornare neque consectetur varius. Maecenas rhoncus consectetur rutrum.</p>
-                </li>
-                <li class="px-6 py-8 border-bottom" data-path="0.2.0.2.0.4">
-                  <button class="w-100 bg-transparent border-0 d-flex justify-content-between align-items-center text-start text-dark" data-path="0.2.0.2.0.4.0">
-                    <span class="h4" data-config-id="text20" data-path="0.2.0.2.0.4.0.0">Cras at ante non ligula pharetra elementum?</span>
-                    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" data-config-id="svg-inline7" data-path="0.2.0.2.0.4.0.1"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.750244 10C0.750244 15.108 4.89124 19.25 10.0002 19.25C15.1082 19.25 19.2502 15.108 19.2502 10C19.2502 4.892 15.1082 0.75 10.0002 0.75C4.89124 0.75 0.750244 4.892 0.750244 10Z" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.4.0.1.0"></path><path d="M6.52905 8.55768L10.0001 12.0437L13.4711 8.55768" stroke="#838EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" data-path="0.2.0.2.0.4.0.1.1"></path></svg></button>
-                  <p class="d-none mt-4 text-muted" data-config-id="text14" data-path="0.2.0.2.0.4.1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod orci sed tristique placerat. Fusce in ligula urna. Fusce eget nunc et libero accumsan rutrum quis nec lectus. Quisque luctus sem nibh, quis ornare neque consectetur varius. Maecenas rhoncus consectetur rutrum.</p>
-                </li>
-              </ul></div>
-          </div>
-        </div>
-      </section>
+   <!--Section: FAQ-->
+<section>
+  <h3 class="text-center mb-4 pb-2 text-primary fw-bold">FAQ</h3>
+  <p class="text-center mb-5">
+    Find the answers for the most frequently asked questions below
+  </p>
+
+  <div class="row">
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="far fa-paper-plane text-primary pe-2"></i> A simple
+        question?</h6>
+      <p>
+        <strong><u>Absolutely!</u></strong> We work with top payment companies which guarantees
+        your safety and
+        security. All billing information is stored on our payment processing partner.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-pen-alt text-primary pe-2"></i> A question
+        that is longer then the previous one?</h6>
+      <p>
+        <strong><u>Yes, it is possible!</u></strong> You can cancel your subscription anytime in
+        your account. Once the subscription is
+        cancelled, you will not be charged next month.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-user text-primary pe-2"></i> A simple
+        question?</h6>
+      <p>
+        Currently, we only offer monthly subscription. You can upgrade or cancel your monthly
+        account at any time with no further obligation.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-rocket text-primary pe-2"></i> A simple
+        question?</h6>
+      <p>
+        Yes. Go to the billing section of your dashboard and update your payment information.
+      </p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-home text-primary pe-2"></i> A simple
+        question?</h6>
+      <p><strong><u>Unfortunately no</u>.</strong> We do not issue full or partial refunds for any
+        reason.</p>
+    </div>
+
+    <div class="col-md-6 col-lg-4 mb-4">
+      <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Another
+        question that is longer than usual</h6>
+      <p>
+        Of course! We’re happy to offer a free plan to anyone who wants to try our service.
+      </p>
+    </div>
+  </div>
+</section>
+<!--Section: FAQ-->
 </template>
 <script>
 export default {
