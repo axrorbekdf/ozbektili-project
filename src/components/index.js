07c94ea02b4b5a7946654a472b3a1157081d1fda
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-30 11:07:47 +0500

    deploy github pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 // export {default as Navbar} from './Navbar.vue'
-// export {default as Login} from './Login.vue'
+export {default as Login} from './auth/Login.vue'
 
 export {default as MyFormWizard} from './MyFormWizard.vue'
+
 
