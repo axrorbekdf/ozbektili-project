443127ffc7711679c0955c2ea14e9e0e8d71340c
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-30 15:35:37 +0500

    deploy github pages

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,21 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style>
+textarea:focus, 
+textarea.form-control:focus, 
+input.form-control:focus, 
+select.form-select:focus, 
+input[type=text]:focus, 
+input[type=password]:focus, 
+input[type=email]:focus, 
+input[type=number]:focus, 
+[type=text].form-control:focus, 
+[type=password].form-control:focus, 
+[type=email].form-control:focus, 
+[type=tel].form-control:focus, 
+[contenteditable].form-control:focus {
+  box-shadow: none;
+}
 
 </style>
--- src/components/auth/MyFormWizard.vue
+++ src/components/auth/MyFormWizard.vue
@@ -79,6 +79,9 @@
                                 <option value="3">Three</option>
                             </select>
                         </div>
+                        
+                        
+
                     </div>
                 </div>
             </TabContent>
@@ -132,6 +135,7 @@ import {gettersTypes} from '@/modules/types'
                 country_id: 1,
                 email: '',
                 password: '',
+
             }
     },
     created(){
@@ -174,20 +178,7 @@ import {gettersTypes} from '@/modules/types'
     },
  }
 </script>
+
 <style>
-textarea:focus, 
-textarea.form-control:focus, 
-input.form-control:focus, 
-select.form-select:focus, 
-input[type=text]:focus, 
-input[type=password]:focus, 
-input[type=email]:focus, 
-input[type=number]:focus, 
-[type=text].form-control:focus, 
-[type=password].form-control:focus, 
-[type=email].form-control:focus, 
-[type=tel].form-control:focus, 
-[contenteditable].form-control:focus {
-  box-shadow: none;
-}
+
 </style>
