d88b88de47135b6c4f672e2f48b8db43d894493a
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-04 10:01:03 +0500

    deploy github pages

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,98 @@
+<template>
+    <nav class="navbar navbar-expand-lg" aria-label="Offcanvas navbar large">
+        <div class="container">
+            <div class="col-md-3 mb-2 mb-md-0" style="text-align: left;">
+            <a href="#" @click="toHomeHandler" class="d-inline-flex link-body-emphasis text-decoration-none ">
+                <img src="@/assets/logo-white.svg" alt="">
+            </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+            <div class="offcanvas-header">
+                <div class="col-md-3 mb-2 mb-md-0" style="text-align: left;">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none ">
+                    <img src="@/assets/logo-white.svg" alt="">
+                </a>
+                </div>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body" style="justify-content: flex-end;">
+                <ul class="nav nav-menu col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <RouterLink to="/" class="nav-link px-2 text-white">Bosh sahifa</RouterLink>
+                <RouterLink to="/platforma-maqsadi" class="nav-link px-2 text-white">Platforma maqsadi</RouterLink>
+                <RouterLink to="/qollanma" class="nav-link px-2 text-white">Qo'llanmalar</RouterLink>
+                <RouterLink to="/about" class="nav-link px-2 text-white">Bog'lanish</RouterLink>
+                </ul>
+
+                <div class="col-md-auto text-end">
+                <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div class="btn-group me-2" role="group" aria-label="First group">
+                    <a href="/" class="btn d-inline-flex link-body-emphasis text-decoration-none ">
+                        <img src="@/assets/flag.svg" alt="">
+                    </a>
+
+                    <a href="/" class="btn d-inline-flex link-body-emphasis text-decoration-none ">
+                        <img src="@/assets/blazon.svg" alt="">
+                    </a>
+
+                    <a href="/" class="btn d-inline-flex link-body-emphasis text-decoration-none ">
+                        <img src="@/assets/white-music.svg" alt="">
+                    </a>
+                    </div>
+
+                    <div class="btn-group me-2" role="group" aria-label="First group">
+                    <button type="button" class="btn btn-primary me-2 text-white dropdown-toggle" data-bs-toggle="dropdown">
+                        <img src="@/assets/local/uzb.png" class="left-image" alt="">
+                        O'zbekcha
+                    </button>
+                    <ul class="dropdown-menu" style="min-width: 4vw; background-color: #0a58ca;">
+                        <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="@/assets/local/en.png" class="left-image" alt="">
+                            English
+                        </a>
+                        </li>
+                        <li>
+                        <a class="dropdown-item" href="#">
+                            <img src="@/assets/local/ru.png" class="left-image" alt="">
+                            Русский
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                    <div v-if="!isLoggedIn" class="btn-group me-2" role="group" aria-label="First group">
+                    <button type="button" class="btn btn-primary text-white">
+                        <RouterLink to="/login" class="nav-link px-2 text-white btn btn-primary">Tizimga kirish</RouterLink>
+                    </button>
+                    <!-- <button type="button" class="btn btn-primary text-white">
+                        <RouterLink to="/register" class="nav-link px-2 text-white btn btn-primary">Ro'yxatdan o'tish</RouterLink>
+                    </button> -->
+                    </div>
+                    <div v-else class="btn-group me-2" role="group" aria-label="Second group">
+                    <button type="button" class="btn btn-primary me-2 text-white  dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="@/assets/user-solid.svg" alt="">
+                        {{ currentUser.name }}
+                    </button>
+                    <ul class="dropdown-menu" style="background-color: #0a58ca;">
+                        <RouterLink to="/lesson" class="dropdown-item">Profile</RouterLink>
+                        <li><a @click="logout" class="dropdown-item" href="#">Chiqish</a></li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
--- src/views/BoglanishView.vue
+++ src/views/BoglanishView.vue
@@ -100,29 +100,29 @@
       <div class="row text-center">
         <div class="col-sm-4">
           <div class="contact-detail-box">
-            <i class="fa fa-th fa-3x text-colored"></i>
-            <h4>Get In Touch</h4>
-            <abbr title="Phone">P:</abbr> (123) 456-7890<br>
-            E: <a href="mailto:email@email.com" class="text-muted">email@email.com</a>
+            <i class="fa mb-4 fa-th fa-3x"></i>
+            <h4>Bog'lanish uchun</h4>
+            Telefon: +(998) 99 123-45-67<br>
+            Email: <a href="mailto:email@email.com" class="text-muted">email@email.com</a>
           </div>
         </div><!-- end col -->
 
         <div class="col-sm-4">
           <div class="contact-detail-box">
-            <i class="fa fa-map-marker fa-3x text-colored"></i>
-            <h4>Our Location</h4>
+            <i class="fa mb-4 fa-map-marker fa-3x"></i>
+            <h4>Manzil</h4>
 
             <address>
-            795 Folsom Ave, Suite 600<br>
-            San Francisco, CA 94107<br>
+              00066, Toshkent sh., Islom <br>
+              Karimov ko'chasi, 45-uy
           </address>
           </div>
         </div><!-- end col -->
 
         <div class="col-sm-4">
           <div class="contact-detail-box">
-            <i class="fa fa-book fa-3x text-colored"></i>
-            <h4>24x7 Support</h4>
+            <i class="fa mb-4 fa-book fa-3x"></i>
+            <h4>24x7</h4>
 
             <p>Industry's standard dummy text.</p>
             <h4 class="text-muted">1234 567 890</h4>
@@ -144,32 +144,32 @@
         <div class="col-sm-6">
           <form role="form" name="ajax-form" id="ajax-form" action="https://formsubmit.io/send/coderthemes@gmail.com" method="post" class="form-main">
 
-            <div class="form-group">
-              <label for="name2">Name</label>
-              <input class="form-control" id="name2" name="name" onblur="if(this.value == '') this.value='Name'" onfocus="if(this.value == 'Name') this.value=''" type="text" value="Name">
+            <div class="form-group mb-2">
+              <label for="name2">Ism</label>
+              <input class="form-control" id="name2" name="name" type="text">
               <div class="error" id="err-name" style="display: none;">Please enter name</div>
             </div> <!-- /Form-name -->
 
-            <div class="form-group">
+            <div class="form-group mb-2">
               <label for="email2">Email</label>
-              <input class="form-control" id="email2" name="email" type="text" onfocus="if(this.value == 'E-mail') this.value='';" onblur="if(this.value == '') this.value='E-mail';" value="E-mail">
+              <input class="form-control" id="email2" name="email" type="text">
               <div class="error" id="err-emailvld" style="display: none;">E-mail is not a valid format</div> 
             </div> <!-- /Form-email -->
 
-            <div class="form-group">
-              <label for="message2">Message</label>
-              <textarea class="form-control" id="message2" name="message" rows="5" onblur="if(this.value == '') this.value='Message'" onfocus="if(this.value == 'Message') this.value=''">Message</textarea>
+            <div class="form-group mb-2">
+              <label for="message2">Xabar</label>
+              <textarea class="form-control" id="message2" name="message" rows="5" ></textarea>
 
               <div class="error" id="err-message" style="display: none;">Please enter message</div>
             </div> <!-- /col -->
 
-            <div class="row">            
+            <div class="row mt-3">            
               <div class="col-xs-12">
-                <div id="ajaxsuccess" class="text-success">E-mail was successfully sent.</div>
+                <!-- <div id="ajaxsuccess" class="text-success">E-mail was successfully sent.</div>
                 <div class="error" id="err-form" style="display: none;">There was a problem validating the form please check!</div>
                 <div class="error" id="err-timedout">The connection to the server timed out!</div>
-                <div class="error" id="err-state"></div>
-                <button type="submit" class="btn btn-primary btn-shadow btn-rounded w-md" id="send">Submit</button>
+                <div class="error" id="err-state"></div> -->
+                <button type="submit" class="btn btn-primary btn-shadow btn-rounded w-md" id="send">Jo'natish</button>
               </div> <!-- /col -->
             </div> <!-- /row -->
 
@@ -183,7 +183,7 @@
       </div>
     </section>
 
-    <section class="container-fluid p-0 mt-5" style="background-color: #062A49">
+    <section class="container-fluid p-0 mt-0" style="background-color: #062A49">
       <div class="container py-5">
         <div class="row justify-content-between py-5">
           <div class="col-md-6 col-12 text-white" style="display: flex; flex-direction: column;">
@@ -292,7 +292,7 @@
 }
 
 #header-bg{
-  height: 50vh;
+  height: 60vh;
   background-color: rgb(0 0 0 / 70%);
 }
 
