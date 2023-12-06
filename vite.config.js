0e999207c1658cb389bb0cee769a417fc2c235a2
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-06 17:54:54 +0500

    deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ozbektili-project",
+  base: "/ozbektili-project", 
   // base: "/",
   plugins: [
     vue(),
