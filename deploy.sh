726a5eeb6bbd96d49c38569bc96e332b46d9396e
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-12-25 10:32:39 +0500

    deploy github pages

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ npm run build
 
 # navigate into the build output directory
 cd dist
+git remote add origin https://github.com/axrorbekdf/ozbektili-project-run.git
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -27,7 +28,7 @@ git commit -m 'deploy background'
 # git push -f git@github.com:axrorbekdf/axrorbekdf.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:axrorbekdf/ozbektili-project.git master:gh-pages
+git push -f git@github.com:axrorbekdf/ozbektili-project.git master:gh-pages
 git push -f git@github.com/axrorbekdf/ozbektili-project-run.git master
 
 cd -
