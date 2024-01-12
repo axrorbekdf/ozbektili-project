bfd1c9b75aaed325e6e803de833ee567a6337e5c
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-01-12 17:05:48 +0500

    deploy github pages

--- src/views/lessons/1-module/Tab22View.vue
+++ src/views/lessons/1-module/Tab22View.vue
@@ -3,25 +3,30 @@
         <div class="row mt-5">
             <p class="text-danger fw-bold"  style="font-size: 10pt;">{{ error }}</p>
             <h3 class="fw-bold text-primary my-3">Savollarga javob bering.</h3>
+
+
             <div class="col-12">
                 <div class="card mb-3" style="max-width: 900px; background-image: linear-gradient(to bottom, #a2e6f6, #ffffff)">
-                <div class="row g-0 py-4">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <p class="card-text"><h6>1: Zuhra qayerlik? </h6></p>
-                            <p class="card-text"> Zuhra <InputText :id="156"/>.</p>
-                            <p class="card-text"><h6>2: Zebo qayerlik? </h6></p>
-                            <p class="card-text"> Zebo <InputText :id="157"/>.</p>
+                    <div class="row g-0 py-4">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                                <h2 class="card-text">-mi?</h2>
+                                <div class="card-text">
+                                    <h3 class="m-2">
+                                        Ha, ______. 
+                                    </h3>
+                                    <h3 class="m-2">
+                                        Yo‘q, ______ emas.
+                                    </h3>
+                                    
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 p-3"  v-for="item,index in items" :key="index">
-                        <div class="list-group mb-3">
-                            <TextChecked :data="item" />
-                        </div>
-                    </div>
-                </div>
                 </div>
             </div>
+
+            <h3 class="fw-bold text-primary my-3">O‘ylang va toping. Yozing.</h3>
 
             <div class="col-12 d-flex justify-content-between my-3">
                 <button @click="chengeTabView('Tab16View')" class="btn" style="background-color: hsla(265, 11%, 78%, 0.514); color: hsl(264, 81%, 43%);">
--- src/views/lessons/1-module/Tab23View.vue
+++ src/views/lessons/1-module/Tab23View.vue
@@ -2,26 +2,208 @@
     <div class="container">
         <div class="row mt-5">
             <p class="text-danger fw-bold"  style="font-size: 10pt;">{{ error }}</p>
-            <h3 class="fw-bold text-primary my-3">Savollarga javob bering.</h3>
+            <h3 class="fw-bold text-primary my-3">Grammatika</h3>
+
+
             <div class="col-12">
                 <div class="card mb-3" style="max-width: 900px; background-image: linear-gradient(to bottom, #a2e6f6, #ffffff)">
-                <div class="row g-0 py-4">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <p class="card-text"><h6>1: Zuhra qayerlik? </h6></p>
-                            <p class="card-text"> Zuhra <InputText :id="156"/>.</p>
-                            <p class="card-text"><h6>2: Zebo qayerlik? </h6></p>
-                            <p class="card-text"> Zebo <InputText :id="157"/>.</p>
+                    <div class="row g-0 py-4">
+                        <div class="col-md-12">
+                            <div class="card-body">
+                                <h2 class="card-text">-lar</h2>
+                                <div class="card-text d-flex align-items-center"> 
+                                    <img src="@/assets/1bolim/images/3616.png" alt="">
+                                    <h3 class="m-2">
+                                        kitob - kitoblar
+                                    </h3>
+                                    <img src="@/assets/1bolim/images/3617.png" alt="">
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-12 p-3"  v-for="item,index in items" :key="index">
-                        <div class="list-group mb-3">
-                            <TextChecked :data="item" />
-                        </div>
-                    </div>
-                </div>
                 </div>
             </div>
+
+            <h3 class="fw-bold text-primary my-3">O‘ylang va toping. Yozing.</h3>
+
+            <div class="col-md-6 col-12 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/361.jpg" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h6 class="card-title">
+                            - Bu kim? <br>
+                            - Bu farmasevt.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-12 mt-3">
+                <div class="card mx-auto" style="width: 20rem;">
+                    <img src="@/assets/1bolim/images/362.png" class="card-img-top mx-auto mt-2" alt="..." style="width:200px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            - Bular kimlar? <br>
+                            - Bular farmasevtlar.
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-6 col-12 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/363.png" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h6 class="card-title">
+                            - ___ ____? <br>
+                            - U paypoq.
+                        </h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-12 mt-3">
+                <div class="card mx-auto" style="width: 20rem;">
+                    <img src="@/assets/1bolim/images/364.png" class="card-img-top mx-auto mt-2" alt="..." style="width:200px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            - Ular ________? <br>
+                            - ______ ________.
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-md-4 col-4 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/365.png" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h6 class="card-title">
+                            Qo‘shiqchi___
+                        </h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-4 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/366.png" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            Ruchka___
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-4 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/367.png" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            Fil___
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/368.jpg" class="card-img-top mx-auto mt-2" alt="..." style="width:100px">
+                    <div class="card-body text-left">
+                        <h6 class="card-title">
+                            _______
+                        </h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 15rem;">
+                    <img src="@/assets/1bolim/images/369.png" class="card-img-top mx-auto mt-2" alt="..." style="width:230px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            _______
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/3610.png" class="card-img-top mx-auto mt-2" alt="..." style="width:95px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            telefon
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 12rem;">
+                    <img src="@/assets/1bolim/images/3611.png" class="card-img-top mx-auto mt-2" alt="..." style="width:170px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            _______
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 12rem;">
+                    <img src="@/assets/1bolim/images/3612.png" class="card-img-top mx-auto mt-2" alt="..." style="width:160px">
+                    <div class="card-body text-left">
+                        <h6 class="card-title">
+                            _______
+                        </h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/3613.jpg" class="card-img-top mx-auto mt-2" alt="..." style="width:130px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            _______
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/3614.png" class="card-img-top mx-auto mt-2" alt="..." style="width:95px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            telefon
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 col-6 mt-3">
+                <div class="card mx-auto" style="width: 10rem;">
+                    <img src="@/assets/1bolim/images/3615.png" class="card-img-top mx-auto mt-2" alt="..." style="width:85px">
+                    <div class="card-body text-left">
+                        <h5 class="card-title">
+                            _______
+                        </h5>
+                    </div>
+                </div>
+            </div>
+
+
+
 
             <div class="col-12 d-flex justify-content-between my-3">
                 <button @click="chengeTabView('Tab16View')" class="btn" style="background-color: hsla(265, 11%, 78%, 0.514); color: hsl(264, 81%, 43%);">
