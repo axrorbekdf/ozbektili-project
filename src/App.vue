ed2e1d5f15825a339329859667f8d4d95f06de36
Ahrorbek Davronov <axrorbektatu@gmail.com>
2024-01-22 10:34:50 +0500

    deploy github pages

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="fluid-container">
+    <div v-if="isLoading" class="waitLoading fluid-container"></div>
     <RouterView />
   </div>
 </template>
 <script>
 import { RouterLink, RouterView } from 'vue-router'
-
+import { mapState } from 'vuex';
 export default {
   created(){
       this.$store.dispatch('getUser')
       this.$store.dispatch('getCountriesStat');
       this.$store.dispatch('getModules');
-  }
+  },
+  computed:{
+      ...mapState({
+            isLoading: (state) => state.module.isLoading,
+      })
+  },
 }
 </script>
 <style>
@@ -31,5 +37,17 @@ input[type=number]:focus,
   box-shadow: none;
 }
 
+.waitLoading {
+  /* The image used */
+  background-image: url("@/assets/wait-loading.png"), url('@/assets/background.png');
+  /* ,  */
+  /* Full height */
+  height:100vh;
+
+  /* Center and scale the image nicely */
+  background-position: center, center;
+  background-repeat: no-repeat, no-repeat;
+  background-size: auto, cover;
+}
 
 </style>
